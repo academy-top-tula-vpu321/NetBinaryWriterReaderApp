1753ee979f119f37c3d826a9981dc0f9512cb659
maxefimov <efimov.top.academy@gmail.com>
2024-03-24 13:12:00 +0300

    Добавьте файлы проекта.

---
+++ NetBinaryWriterReaderApp/Program.cs
@@ -0,0 +1,19 @@
+﻿string fileName = "file.bin";
+
+int n = 154678;
+double x = 654564.098987;
+bool f = true;
+
+//using(BinaryWriter writer = new BinaryWriter(File.OpenWrite(fileName)))
+//{
+//    writer.Write(n);
+//    writer.Write(x);
+//    writer.Write(f);
+//}
+
+using(BinaryReader reader = new BinaryReader(File.OpenRead(fileName)))
+{
+    Console.WriteLine(reader.ReadInt32());
+    Console.WriteLine(reader.ReadDouble());
+    Console.WriteLine(reader.ReadBoolean());
+}
